5262a454b87feb2146537fceb3086ee0afe60335
tooplick <id6543156918@gmail.com>
2026-05-12 11:34:05 +0800

    Update

--- src/components/VHooks.vue
+++ src/components/VHooks.vue
@@ -1,14 +1,32 @@
 <template>
   <h2>当前求和</h2>
   <h3>{{ count }}</h3>
-  <button @click="count++">+1</button>
+  <button @click="add">+1</button>
+  <hr/>
+  <img :src="dog" v-for="(dog,index) in dogList" :key="index"/>
+  <br/>
+  <button @click="addDog">再来一只</button>
 </template>
 
 <script setup>
+import axios from 'axios';
 import { ref } from 'vue'
 const count = ref(0)
+let dogList = ref(['https://ygking.top/img/cover.jpg'])
+let add = () => {
+  count.value += 1;
+}
+async function addDog() {
+  let result = await axios.get('https://dog.ceo/api/breeds/image/random')
+  dogList.value.push('https://vpn.ygking.top/'+result.data.message)
+}
+
 </script>
 
 <style scoped>
-
+img {
+  width: 200px;
+  height: 200px;
+  margin: 10px;
+}
 </style>

6f70caf0d11212e0ee5512526933652920bc0b6f
tooplick <id6543156918@gmail.com>
2026-05-12 10:53:34 +0800

    Update

---
+++ src/components/VHooks.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>当前求和</h2>
+  <button @click="count++">+1</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<style scoped>
+
+</style>
